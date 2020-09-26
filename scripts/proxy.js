1d51d975c5eafa2a7b14184fb788243b679a41c1
Xiao Chuan <w2239559319@outlook.com>
2020-09-26 22:49:10 +0800

    Update proxy.js

--- scripts/proxy.js
+++ scripts/proxy.js
@@ -1,4 +1,4 @@
-const cookie = 'eai-sess=qcc0vd3gvi690n2a257db4aij5; UUkey=bb44c8935d4009e460bdf57a8003cb0b; Hm_lvt_48b682d4885d22a90111e46b972e3268=1600499592,1600499603,1601044163; Hm_lpvt_48b'
+const cookie = ''
 const proxy = {
   '/': {
     target: 'https://wfw.scu.edu.cn',
